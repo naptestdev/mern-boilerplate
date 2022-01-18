5eaab248717cac97355c1828559ecd3f903b578d
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-01-18 19:10:54 +0700

    v1.0.2
    would this work?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+const path = require('path');
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 export default defineConfig({
   plugins: [viteCommonjs()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'server.js'),
+      name: 'MyLib',
+      fileName: () => `server.js`
+    }
+  }
 });
